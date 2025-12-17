59b180e72fe6738dc8ae741836193ee8c77afd2b
flowmeterproject2026-creator <flowmeterproject2026@gmail.com>
2025-12-17 13:54:21 +0530

    Update latest.js

--- latest.js
+++ latest.js
@@ -1,12 +1,13 @@
+// api/latest.js
 import fs from "fs";
 
 export default function handler(req, res) {
-  const filePath = "/tmp/latest.json";
+  const path = "/tmp/latest.json";
 
-  if (!fs.existsSync(filePath)) {
+  if (!fs.existsSync(path)) {
     return res.status(200).json({ status: "NO_DATA" });
   }
 
-  const data = JSON.parse(fs.readFileSync(filePath));
-  res.status(200).json(data);
+  const json = JSON.parse(fs.readFileSync(path));
+  res.status(200).json(json);
 }
