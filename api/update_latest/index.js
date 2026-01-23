7e70cd8f4366d0b48c4e22a54da0d19e87f5e3f8
flowmeterproject2026-creator <flowmeterproject2026@gmail.com>
2026-01-23 22:35:24 +0530

    Update index.js

--- api/update_latest/index.js
+++ api/update_latest/index.js
@@ -40,10 +40,12 @@ function todayIST() {
 
 export default async function handler(req, res) {
   // ✅ CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.status(200).end();
+ res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "*");
+
+if (req.method === "OPTIONS") return res.status(200).end();
+
 
   // ✅ Mongo check
   if (!MONGODB_URI) {
